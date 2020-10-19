6188d360f12b5e1cf8e1daf3c1d8ceaa3ba32b08
Hardeep Asrani <hardeepasrani@gmail.com>
2020-10-20 04:39:51 +0530

    Fix phplint errors

--- editor/build/index.asset.php
+++ editor/build/index.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => 'e79e836254c5f5f0ce154018f33c6ae4');
+<?php return array(
+	'dependencies' => array( 'wp-element', 'wp-polyfill', 'wp-primitives' ),
+	'version'      => 'e79e836254c5f5f0ce154018f33c6ae4',
+);
