813fc1d010e7a40608d754533912e1e57b966932
cristian-ungureanu <cristian@themeisle.com>
2020-10-22 15:13:53 +0300

    fix: Woo shop page after import

--- templates-patterns-collection.php
+++ templates-patterns-collection.php
@@ -17,6 +17,7 @@
 
 add_action( 'admin_notices', 'ti_tpc_plugins_page_notice' );
 add_action( 'init', 'ti_tpc_load_textdomain' );
+add_action( 'init', 'ti_tpc_flush_premalinks' );
 
 /**
  * Plugins page notice if we don't have neve activated.
@@ -51,6 +52,22 @@ function ti_tpc_plugins_page_notice() {
 	$notice .= '</div>';
 
 	echo wp_kses_post( $notice );
+}
+
+/**
+ * Flush the permalinks after import
+ *
+ * @return bool
+ */
+function ti_tpc_flush_premalinks() {
+	$flash_rules = get_transient( 'ti_tpc_should_flush_permalinks' );
+	if ( $flash_rules !== 'yes' ){
+		return false;
+	}
+	
+	flush_rewrite_rules();
+	delete_transient( 'ti_tpc_should_flush_permalinks' );
+	return true;
 }
 
 /**
