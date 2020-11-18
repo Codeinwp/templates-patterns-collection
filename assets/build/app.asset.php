c6550c77ac97510f1162d65ccbb51ace57c3b81e
abaicus <baicusandrei@me.com>
2020-11-18 12:23:58 +0200

    chore: code style fixes

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'dd5c0487b4a475141786056e0697e428');
+<?php return array(
+	'dependencies' => array( 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives' ),
+	'version'      => 'dd5c0487b4a475141786056e0697e428',
+);
