60b5babf5c1cf27d99ff8072613cae619993fb4b
Hardeep Asrani <hardeepasrani@gmail.com>
2021-01-08 21:34:21 +0530

    format php

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '0ee40562fd795550207e8924338dce8e');
+<?php return array( 'dependencies' => array( 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives' ), 'version' => '0ee40562fd795550207e8924338dce8e' );
--- elementor/build/index.asset.php
+++ elementor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '527f5fafa87755a8d12f1ab800fe961f');
+<?php return array( 'dependencies' => array( 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-keycodes', 'wp-polyfill', 'wp-primitives' ), 'version' => '527f5fafa87755a8d12f1ab800fe961f' );
