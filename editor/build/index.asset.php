b18f667dcf405639ed80ae08dbb408e132058e42
cristian-ungureanu <cristian@themeisle.com>
2021-01-25 10:19:47 +0200

    chore: run php lint

--- editor/build/index.asset.php
+++ editor/build/index.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives'), 'version' => '2f2fd1643cacd976a057d669cb9943fd');
+<?php return array(
+	'dependencies' => array( 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives' ),
+	'version'      => '2f2fd1643cacd976a057d669cb9943fd',
+);
