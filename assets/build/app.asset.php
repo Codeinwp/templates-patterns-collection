ab85716f1be9cf745adeba3caa841c4936030b64
cristian-ungureanu <cristian@themeisle.com>
2021-01-25 10:16:52 +0200

    fix: icon misaligned [#46]

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives' ),
-	'version'      => 'edaeef1d2a5566fc3aa7447a01bcc8c3',
-);
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'edaeef1d2a5566fc3aa7447a01bcc8c3');
