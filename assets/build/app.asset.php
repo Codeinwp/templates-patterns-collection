b076b3f11e84ae9d1acfc4cd2905d719a0653a03
Hardeep Asrani <hardeepasrani@gmail.com>
2021-02-24 16:42:16 +0530

    Fix pagination
    Fix pagination for elementor. Fixes https://github.com/Codeinwp/templates-patterns-collection/issues/58

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '9b40a9efb2cfdfecf8cf6eef034e3827');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '96436d32a7ed002ba435d91dc6550242');
