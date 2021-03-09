f29388466d1cc8e1a366e05a47ea30d9c6088623
Hardeep Asrani <hardeepasrani@gmail.com>
2021-03-10 04:37:18 +0530

    Fix no templates showing after deleting
    Fixes https://github.com/Codeinwp/templates-patterns-collection/issues/71

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '96436d32a7ed002ba435d91dc6550242');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'c790b0131a835de1febe010839809a8d');
