f61e3d84da0e8221560e25a72f1f4bace98d7091
Hardeep Asrani <hardeepasrani@gmail.com>
2021-03-17 00:28:44 +0530

    Rebuild code

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'a5e713ee9a00b6dd8c6281469864c8f0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'db1be32eb732b1b307c1487e18c16fcc');
