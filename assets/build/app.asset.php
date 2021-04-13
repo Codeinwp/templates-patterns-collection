5b18cdb8b51e47930117ad19d7b380dc2b5335b5
Hardeep Asrani <hardeepasrani@gmail.com>
2021-04-14 04:39:15 +0530

    Decode HTML entities in title
    fixes https://github.com/Codeinwp/templates-patterns-collection/issues/24

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'b0a1bd2c51432c35566ff951a4a8134e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'f3d7dbdc0f44c1f2a0750b1bb789c18f');
