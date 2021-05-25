8ebc43aa6b6711baaaab58022fe0e44c96b69aa5
Bogdan Preda <bogdan.preda@themeisle.com>
2021-05-25 16:18:25 +0300

    test: test full store import

---
+++ tests/digital-test.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * Digital Store Test.
+ *
+ * @package templates-patterns-collection
+ */
+
+use TIOB\Importers\Widgets_Importer;
+use TIOB\Main;
+use TIOB\Rest_Server;
+
+/**
+ * Mock Class WooCommerce to pass class check for individual tests
+ */
+class WooCommerce {}
+
+/**
+ * Test Digital Store import.
+ */
+class Digital_Rest_Test extends WP_UnitTestCase {
+	public static $admin_id;
+
+	/**
+	 * @var Rest_Server
+	 */
+	private $rest_api;
+
+	/**
+	 * @var string
+	 */
+	private $json;
+
+	/**
+	 * @var Widgets_Importer
+	 */
+	private $widget_importer;
+
+	/**
+	 * @param $factory
+	 */
+	public static function wpSetUpBeforeClass( $factory ) {
+		self::$admin_id = $factory->user->create( array( 'role' => 'administrator' ) );
+	}
+
+	/**
+	 * setUp
+	 */
+	public function setUp() {
+		parent::setUp();
+		wp_set_current_user( self::$admin_id );
+		add_theme_support(
+			'themeisle-demo-import',
+			array(
+				'editors' => array(
+					'gutenberg',
+				),
+				'local'   => array(
+					'elementor' => array(
+						'neve-main' => array(
+							'url'   => 'https://demo.themeisle.com/neve',
+							'title' => 'Neve 2018',
+						),
+					),
+				),
+				'i18n'    => array(
+					'templates_title'       => __( 'Ready to use pre-built websites with 1-click installation', 'templates-patterns-collection' ),
+					'templates_description' => __( 'With Neve, you can choose from multiple unique demos, specially designed for you, that can be installed with a single click. You just need to choose your favorite, and we will take care of everything else.', 'templates-patterns-collection' ),
+				),
+			)
+		);
+
+		Main::instance();
+
+		$this->json     = json_decode( file_get_contents( dirname( __FILE__ ) . '/fixtures/digital_store/data.json' ), true );
+		$this->rest_api = new Rest_Server();
+		$this->rest_api->init();
+
+		require_once dirname( __FILE__ ) . '/../includes/Importers/Widgets_Importer.php';
+		$this->widget_importer = new Widgets_Importer();
+		add_filter( 'async_update_translation', '__return_false' );
+	}
+
+	//  /**
+	//   * Test all theme_mods import form the json data.
+	//   *
+	//   * @covers \TIOB\Importers\Theme_Mods_Importer::import_theme_mods
+	//   */
+	//  public function test_theme_mods_importing() {
+	//
+	//      $request = new WP_REST_Request();
+	//
+	//      $request->set_header( 'content-type', 'application/json' );
+	//      $request->set_method( 'POST' );
+	//      $request->set_body(
+	//          json_encode(
+	//              array(
+	//                  'theme_mods' => $this->json['theme_mods'],
+	//                  'source_url' => 'https://demo.themeisle.com/digital-store-gb',
+	//              )
+	//          )
+	//      );
+	//
+	//      $response = $this->rest_api->run_theme_mods_importer( $request );
+	//
+	//      $this->assertInstanceOf( 'WP_REST_Response', $response );
+	//      $this->assertEquals( 200, $response->get_status() );
+	//      $this->assertTrue( $response->get_data()['success'] );
+	//      //Test that ALL theme mods have been successfully imported.
+	//      foreach ( $this->json['theme_mods'] as $key => $value ) {
+	//          $this->assertEquals( $value, get_theme_mod( $key ) );
+	//      }
+	//  }
+	//
+	//  /**
+	//   * Test all plugin imports from the JSON data.
+	//   *
+	//   * @covers \TIOB\Importers\Plugin_Importer::install_plugins
+	//   */
+	//  public function test_plugin_importing() {
+	//      $request = new WP_REST_Request();
+	//
+	//      $request->set_header( 'content-type', 'application/json' );
+	//      $request->set_method( 'POST' );
+	//      $plugins_to_install = array_merge( $this->json['mandatory_plugins'], $this->json['recommended_plugins'] );
+	//      $plugins_to_install = array_map(
+	//          function( $val ) {
+	//              return true;
+	//          },
+	//          $plugins_to_install
+	//      );
+	//      $request->set_body(
+	//          json_encode(
+	//              $plugins_to_install
+	//          )
+	//      );
+	//
+	//      $response = $this->rest_api->run_plugin_importer( $request );
+	//
+	//      $this->assertInstanceOf( 'WP_REST_Response', $response );
+	//      $this->assertEquals( 200, $response->get_status() );
+	//      $this->assertTrue( $response->get_data()['success'] );
+	//      //Test that ALL plugins have been installed and activated.
+	//      $plugin_list = get_plugins();
+	//      foreach ( $plugins_to_install as $plugin_slug => $value ) {
+	//          foreach ( $plugin_list as $key => $data ) {
+	//              if ( isset( $data['Name'] ) && sanitize_title( $data['Name'] ) === $plugin_slug ) {
+	//                  $this->assertTrue( is_plugin_active( $key ) );
+	//              }
+	//          }
+	//      }
+	//      // Test that the amount of plugins to install is contained inside the installed plugins.
+	//      $this->assertTrue( count( $plugins_to_install ) <= count( $plugin_list ) );
+	//  }
+
+
+	/**
+	 * Test the Content Import form the store.xml
+	 *
+	 * @covers \TIOB\Importers\Content_Importer::import_remote_xml
+	 */
+	public function test_content_import() {
+		$request = new WP_REST_Request();
+
+		$request->set_header( 'content-type', 'application/json' );
+		$request->set_method( 'POST' );
+		$content_import = array(
+			'demoSlug'    => 'neve-digital-store-gutenberg',
+			'editor'      => 'gutenberg',
+			'source'      => 'local',
+			'contentFile' => dirname( __FILE__ ) . '/fixtures/digital_store/export.xml',
+			'frontPage'   => $this->json['front_page'],
+			'shopPages'   => $this->json['shop_pages'],
+		);
+		$request->set_body( json_encode( $content_import ) );
+
+		$response = $this->rest_api->run_xml_importer( $request );
+		$this->assertInstanceOf( 'WP_REST_Response', $response );
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertTrue( $response->get_data()['success'] );
+
+		//Test that front page has been set up.
+		$this->assertEquals( 'page', get_option( 'show_on_front' ) );
+		//Test that front page WP_Post object exists and is set as front page.
+		$front_page_id = get_option( 'page_on_front' );
+		$front_page    = get_post( $front_page_id );
+		$this->assertInstanceOf( 'WP_Post', $front_page );
+		$this->assertNotEmpty( $front_page->ID );
+
+		//Test that blog page WP_Post object exists and is set as page for posts.
+		$blog_page_id = get_option( 'page_for_posts' );
+		$blog_page    = get_post( $blog_page_id );
+		$this->assertInstanceOf( 'WP_Post', $blog_page );
+		$this->assertNotEmpty( $blog_page->ID );
+
+		foreach ( $this->json['shop_pages'] as $option_id => $slug ) {
+			if ( ! empty( $slug ) ) {
+				$shop_page_id = get_option( $option_id );
+				$shop_page    = get_post( $shop_page_id );
+				$this->assertInstanceOf( 'WP_Post', $shop_page );
+				$this->assertNotEmpty( $shop_page->ID );
+			}
+		}
+	}
+
+
+	/**
+	 * @covers \TIOB\Importers\Widgets_Importer::import_widgets
+	 */
+	public function test_widgets_import() {
+		$this->remove_all_widgets();
+
+		$request = new WP_REST_Request();
+		$request->set_header( 'content-type', 'application/json' );
+		$request->set_method( 'POST' );
+		$request->set_body(
+			json_encode(
+				$this->json['widgets']
+			)
+		);
+
+		$response = $this->rest_api->run_widgets_importer( $request );
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertTrue( $response->get_data()['success'] );
+		$this->assertInstanceOf( 'WP_REST_Response', $response );
+
+		$available_widgets = $this->widget_importer->available_widgets();
+		$widget_instances  = array();
+		foreach ( $available_widgets as $widget_data ) {
+			$instances = get_option( 'widget_' . $widget_data['id_base'] );
+			if ( ! empty( $instances ) ) {
+				foreach ( $instances as $instance_id => $instance_data ) {
+					if ( is_numeric( $instance_id ) ) {
+						$unique_instance_id                      = $widget_data['id_base'] . '-' . $instance_id;
+						$widget_instances[ $unique_instance_id ] = $instance_data;
+					}
+				}
+			}
+		}
+
+		$accepted_titles = array();
+		foreach ( $this->json['widgets'] as $widget_location ) {
+			foreach ( $widget_location as $widget ) {
+				if ( ! empty( $widget['title'] ) ) {
+					array_push( $accepted_titles, $widget['title'] );
+				}
+			}
+		}
+
+		foreach ( $widget_instances as $widget_instance ) {
+			$this->assertTrue( isset( $widget_instance['title'] ) );
+			$this->assertTrue( in_array( $widget_instance['title'], $accepted_titles ) || $widget_instance['title'] === '' );
+		}
+	}
+
+	/**
+	 * Empty site widgets.
+	 */
+	private function remove_all_widgets() {
+		$available_widgets = $this->widget_importer->available_widgets();
+		foreach ( $available_widgets as $widget_data ) {
+			delete_option( 'widget_' . $widget_data['id_base'] );
+		}
+	}
+}
--- tests/import-test.php
+++ tests/import-test.php
@@ -26,7 +26,7 @@ class Import_Test extends WP_UnitTestCase {
 				'query_var'         => is_admin(),
 				'rewrite'           => false,
 				'public'            => false,
-				'label'             => _x( 'Product type', 'Taxonomy name', 'woocommerce' ),
+				'label'             => _x( 'Product type', 'Taxonomy name', 'default' ),
 			)
 		);
 
@@ -40,7 +40,7 @@ class Import_Test extends WP_UnitTestCase {
 				'query_var'         => is_admin(),
 				'rewrite'           => false,
 				'public'            => false,
-				'label'             => _x( 'Product visibility', 'Taxonomy name', 'woocommerce' ),
+				'label'             => _x( 'Product visibility', 'Taxonomy name', 'default' ),
 			)
 		);
 
@@ -50,20 +50,20 @@ class Import_Test extends WP_UnitTestCase {
 			array(
 				'hierarchical'          => true,
 				'update_count_callback' => '_wc_term_recount',
-				'label'                 => __( 'Categories', 'woocommerce' ),
+				'label'                 => __( 'Categories', 'default' ),
 				'labels'                => array(
-					'name'              => __( 'Product categories', 'woocommerce' ),
-					'singular_name'     => __( 'Category', 'woocommerce' ),
-					'menu_name'         => _x( 'Categories', 'Admin menu name', 'woocommerce' ),
-					'search_items'      => __( 'Search categories', 'woocommerce' ),
-					'all_items'         => __( 'All categories', 'woocommerce' ),
-					'parent_item'       => __( 'Parent category', 'woocommerce' ),
-					'parent_item_colon' => __( 'Parent category:', 'woocommerce' ),
-					'edit_item'         => __( 'Edit category', 'woocommerce' ),
-					'update_item'       => __( 'Update category', 'woocommerce' ),
-					'add_new_item'      => __( 'Add new category', 'woocommerce' ),
-					'new_item_name'     => __( 'New category name', 'woocommerce' ),
-					'not_found'         => __( 'No categories found', 'woocommerce' ),
+					'name'              => __( 'Product categories', 'default' ),
+					'singular_name'     => __( 'Category', 'default' ),
+					'menu_name'         => _x( 'Categories', 'Admin menu name', 'default' ),
+					'search_items'      => __( 'Search categories', 'default' ),
+					'all_items'         => __( 'All categories', 'default' ),
+					'parent_item'       => __( 'Parent category', 'default' ),
+					'parent_item_colon' => __( 'Parent category:', 'default' ),
+					'edit_item'         => __( 'Edit category', 'default' ),
+					'update_item'       => __( 'Update category', 'default' ),
+					'add_new_item'      => __( 'Add new category', 'default' ),
+					'new_item_name'     => __( 'New category name', 'default' ),
+					'not_found'         => __( 'No categories found', 'default' ),
 				),
 				'show_ui'               => true,
 				'query_var'             => true,
@@ -87,22 +87,22 @@ class Import_Test extends WP_UnitTestCase {
 			array(
 				'hierarchical'          => false,
 				'update_count_callback' => '_wc_term_recount',
-				'label'                 => __( 'Product tags', 'woocommerce' ),
+				'label'                 => __( 'Product tags', 'default' ),
 				'labels'                => array(
-					'name'                       => __( 'Product tags', 'woocommerce' ),
-					'singular_name'              => __( 'Tag', 'woocommerce' ),
-					'menu_name'                  => _x( 'Tags', 'Admin menu name', 'woocommerce' ),
-					'search_items'               => __( 'Search tags', 'woocommerce' ),
-					'all_items'                  => __( 'All tags', 'woocommerce' ),
-					'edit_item'                  => __( 'Edit tag', 'woocommerce' ),
-					'update_item'                => __( 'Update tag', 'woocommerce' ),
-					'add_new_item'               => __( 'Add new tag', 'woocommerce' ),
-					'new_item_name'              => __( 'New tag name', 'woocommerce' ),
-					'popular_items'              => __( 'Popular tags', 'woocommerce' ),
-					'separate_items_with_commas' => __( 'Separate tags with commas', 'woocommerce' ),
-					'add_or_remove_items'        => __( 'Add or remove tags', 'woocommerce' ),
-					'choose_from_most_used'      => __( 'Choose from the most used tags', 'woocommerce' ),
-					'not_found'                  => __( 'No tags found', 'woocommerce' ),
+					'name'                       => __( 'Product tags', 'default' ),
+					'singular_name'              => __( 'Tag', 'default' ),
+					'menu_name'                  => _x( 'Tags', 'Admin menu name', 'default' ),
+					'search_items'               => __( 'Search tags', 'default' ),
+					'all_items'                  => __( 'All tags', 'default' ),
+					'edit_item'                  => __( 'Edit tag', 'default' ),
+					'update_item'                => __( 'Update tag', 'default' ),
+					'add_new_item'               => __( 'Add new tag', 'default' ),
+					'new_item_name'              => __( 'New tag name', 'default' ),
+					'popular_items'              => __( 'Popular tags', 'default' ),
+					'separate_items_with_commas' => __( 'Separate tags with commas', 'default' ),
+					'add_or_remove_items'        => __( 'Add or remove tags', 'default' ),
+					'choose_from_most_used'      => __( 'Choose from the most used tags', 'default' ),
+					'not_found'                  => __( 'No tags found', 'default' ),
 				),
 				'show_ui'               => true,
 				'query_var'             => true,
@@ -125,19 +125,19 @@ class Import_Test extends WP_UnitTestCase {
 			array(
 				'hierarchical'          => false,
 				'update_count_callback' => '_update_post_term_count',
-				'label'                 => __( 'Shipping classes', 'woocommerce' ),
+				'label'                 => __( 'Shipping classes', 'default' ),
 				'labels'                => array(
-					'name'              => __( 'Product shipping classes', 'woocommerce' ),
-					'singular_name'     => __( 'Shipping class', 'woocommerce' ),
-					'menu_name'         => _x( 'Shipping classes', 'Admin menu name', 'woocommerce' ),
-					'search_items'      => __( 'Search shipping classes', 'woocommerce' ),
-					'all_items'         => __( 'All shipping classes', 'woocommerce' ),
-					'parent_item'       => __( 'Parent shipping class', 'woocommerce' ),
-					'parent_item_colon' => __( 'Parent shipping class:', 'woocommerce' ),
-					'edit_item'         => __( 'Edit shipping class', 'woocommerce' ),
-					'update_item'       => __( 'Update shipping class', 'woocommerce' ),
-					'add_new_item'      => __( 'Add new shipping class', 'woocommerce' ),
-					'new_item_name'     => __( 'New shipping class Name', 'woocommerce' ),
+					'name'              => __( 'Product shipping classes', 'default' ),
+					'singular_name'     => __( 'Shipping class', 'default' ),
+					'menu_name'         => _x( 'Shipping classes', 'Admin menu name', 'default' ),
+					'search_items'      => __( 'Search shipping classes', 'default' ),
+					'all_items'         => __( 'All shipping classes', 'default' ),
+					'parent_item'       => __( 'Parent shipping class', 'default' ),
+					'parent_item_colon' => __( 'Parent shipping class:', 'default' ),
+					'edit_item'         => __( 'Edit shipping class', 'default' ),
+					'update_item'       => __( 'Update shipping class', 'default' ),
+					'add_new_item'      => __( 'Add new shipping class', 'default' ),
+					'new_item_name'     => __( 'New shipping class Name', 'default' ),
 				),
 				'show_ui'               => false,
 				'show_in_quick_edit'    => false,
@@ -162,7 +162,7 @@ class Import_Test extends WP_UnitTestCase {
 			}
 		}
 
-		$alterator = new Importer_Alterator( [ "demoSlug" => "testImport" ] );
+		$alterator = new Importer_Alterator( array( "demoSlug" => "testImport" ) );
 
 
 		$export_file_path = dirname( __FILE__ ) . '/fixtures/digital_store/export.xml';
@@ -171,7 +171,7 @@ class Import_Test extends WP_UnitTestCase {
 		$importer->import( $export_file_path );
 
 		$post = get_page_by_title( 'Home', OBJECT, 'page' );
-		$re = '/categoryId":(?<categoryID>\d+)/m';
+		$re   = '/categoryId":(?<categoryID>\d+)/m';
 		preg_match_all( $re, $post->post_content, $matches );
 		foreach ( $matches['categoryID'] as $match ) {
 			$this->assertTrue( in_array( $match, $importer->processed_terms ) );
