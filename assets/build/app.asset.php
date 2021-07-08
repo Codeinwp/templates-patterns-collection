4c45392454a1b8ca070aaa2c75fd0db8a5382364
Bogdan Preda <bogdan.preda@themeisle.com>
2021-07-08 14:12:04 +0300

    chore: build files

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '675aed134e9f970a27826c2f43ddf317');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'de5e087761282f87cd9ea72ff9164abd');
