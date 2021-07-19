b083e09fce9aaca793bcc3c0b33faccc59a39823
Bogdan Preda <bogdan.preda@themeisle.com>
2021-07-19 13:43:23 +0300

    fix: refresh error bug #109

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '441e987a4e4fa52caf044111dc2be02b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'ad4eaaab86c1f84ba7bbe20d00250f1e');
