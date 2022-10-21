a2234bb39f061b8955b00abcd67ae0cfa56ab4e0
Bogdan Preda <bogdan.preda@themeisle.com>
2022-10-21 12:50:20 +0300

    chore: revert function change

--- includes/Admin.php
+++ includes/Admin.php
@@ -197,7 +197,7 @@ class Admin {
 				'license_id' => apply_filters( 'tiob_license_key', 'free' ),
 			),
 			'upsellNotifications' => $this->get_upsell_notifications(),
-			'isValidLicense'      => License::has_active_license(),
+			'isValidLicense'      => $this->has_valid_addons(),
 			'licenseTIOB'         => License::get_license_data(),
 		);
 	}
@@ -478,4 +478,34 @@ class Admin {
 			),
 		);
 	}
+
+	/**
+	 * Check validity of addons plugin.
+	 *
+	 * @return bool
+	 */
+	private function has_valid_addons() {
+		if ( ! defined( 'NEVE_PRO_BASEFILE' ) ) {
+			return false;
+		}
+
+		$option_name = basename( dirname( NEVE_PRO_BASEFILE ) );
+		$option_name = str_replace( '-', '_', strtolower( trim( $option_name ) ) );
+		$status      = get_option( $option_name . '_license_data' );
+
+		if ( ! $status ) {
+			return false;
+		}
+
+		if ( ! isset( $status->license ) ) {
+			return false;
+		}
+
+		if ( $status->license === 'not_active' || $status->license === 'invalid' ) {
+			return false;
+		}
+
+		return true;
+	}
+
 }
