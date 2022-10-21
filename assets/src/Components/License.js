d1fbeaeb483ed15c5ec11b5d0195e5ac7c99f3ed
Bogdan Preda <bogdan.preda@themeisle.com>
2022-10-21 12:33:41 +0300

    chore: remove dependency

--- assets/src/Components/License.js
+++ assets/src/Components/License.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from '@wordpress/element';
+import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
 import { withDispatch, withSelect } from '@wordpress/data';
