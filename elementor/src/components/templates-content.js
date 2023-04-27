18b8931a86d559084c4238d205bf536df277695c
Bogdan Preda <bogdan.preda@themeisle.com>
2023-04-27 20:55:29 +0300

    fix: improved elementor styles for light and dark skin #239

--- elementor/src/components/templates-content.js
+++ elementor/src/components/templates-content.js
@@ -150,7 +150,7 @@ const TemplatesContent = ( {
 				( isGeneral ? (
 					<p>{ 'No templates found. Check again later!' }</p>
 				) : (
-					<p>
+					<p style={ { alignSelf: 'center' } }>
 						{ 'No templates available. Try adding few templates.' }
 					</p>
 				) ) }
