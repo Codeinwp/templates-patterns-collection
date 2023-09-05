748f1142224c217122b89af9688c21fb16118833
cristian-ungureanu <cristian@themeisle.com>
2023-09-05 17:42:16 +0300

    chore: add condition for loading onboarding

--- includes/Admin.php
+++ includes/Admin.php
@@ -468,7 +468,7 @@ class Admin {
 			return;
 		}
 
-		if ( $this->neve_new_user === 'yes' && strpos( $screen->id, '_page_neve-onboarding' ) ) {
+		if ( $this->neve_new_user === 'yes' && current_user_can( 'install_plugins') && strpos( $screen->id, '_page_neve-onboarding' ) ) {
 
 			wp_enqueue_media();
 
