9802c37b0b9655b3f9fce20d650977897b422574
cristian-ungureanu <cristian@themeisle.com>
2023-09-11 20:08:25 +0300

    chore: required changes

--- onboarding/src/Components/CustomTooltip.js
+++ onboarding/src/Components/CustomTooltip.js
@@ -8,15 +8,15 @@ const CustomTooltip = ( {
 	toLeft = false,
 	rightOffset = 0,
 } ) => {
-	const tooltipClassNames = classnames( [ className, 'tiob-tooltip-wrap' ] );
+	const tooltipClassNames = classnames( [ className, 'ob-tooltip-wrap' ] );
 
 	const ttStyle = toLeft ? { right: rightOffset, left: 'unset' } : {};
 
 	return (
 		<div className={ tooltipClassNames }>
-			<div className="tiob-tooltip-toggle">
+			<div className="ob-tooltip-toggle">
 				<Icon icon={ info } size={ 24 } />
-				<div className="tiob-tooltip-content" style={ ttStyle }>
+				<div className="ob-tooltip-content" style={ ttStyle }>
 					{ children }
 				</div>
 			</div>
--- onboarding/src/Components/EditorSelector.js
+++ onboarding/src/Components/EditorSelector.js
@@ -1,77 +1,78 @@
 /* global tiobDash */
 import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
-import { Button, Dashicon, Popover } from '@wordpress/components';
+import { Button, Dashicon } from '@wordpress/components';
 import { withDispatch, withSelect } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
+import classnames from 'classnames';
 
 const EditorSelector = ( { EDITOR_MAP, editor, setCurrentEditor, sites } ) => {
 	const [ open, setOpen ] = useState( false );
 	const editorsOrderedFromAPI = Object.keys( sites );
 	const toggleDropdown = () => setOpen( ! open );
+
+	const toggleButtonClasses = classnames(
+		'select',
+		'ob-dropdown',
+		open ? 'active' : ''
+	);
 	return (
 		<div className="ob-editor-selector">
-			<Button onClick={ toggleDropdown } className="select ob-dropdown">
-				<img
-					className="editor-icon"
-					src={ tiobDash.assets + 'img/' + EDITOR_MAP[ editor ].icon }
-					alt={ __(
-						'Builder Logo',
-						'templates-patterns-collection'
-					) }
-				/>
-				<Dashicon
-					size={ 14 }
-					icon={ open ? 'arrow-up-alt2' : 'arrow-down-alt2' }
-				/>
-				{ open && (
-					<Popover
-						position="bottom right"
-						onClose={ toggleDropdown }
-						noArrow
-						offset={ 10 }
-					>
-						{ open && (
-							<ul className="options">
-								{ editorsOrderedFromAPI.map( ( key, index ) => {
-									if ( key === editor ) {
-										return null;
-									}
-									return (
-										<li key={ index }>
-											<a
-												href="#"
-												onClick={ ( e ) => {
-													e.preventDefault();
-													setCurrentEditor( key );
-													setOpen( false );
-												} }
-											>
-												<img
-													className="editor-icon"
-													src={
-														tiobDash.assets +
-														'img/' +
-														EDITOR_MAP[ key ].icon
-													}
-													alt={ __(
-														'Builder Logo',
-														'templates-patterns-collection'
-													) }
-												/>
-												<span>
-													{
-														EDITOR_MAP[ key ]
-															.niceName
-													}
-												</span>
-											</a>
-										</li>
-									);
-								} ) }
-							</ul>
+			<Button
+				onClick={ toggleDropdown }
+				className={ toggleButtonClasses }
+			>
+				<span className="ob-current-editor">
+					<img
+						className="editor-icon"
+						src={
+							tiobDash.assets + 'img/' + EDITOR_MAP[ editor ].icon
+						}
+						alt={ __(
+							'Builder Logo',
+							'templates-patterns-collection'
 						) }
-					</Popover>
+					/>
+					<Dashicon
+						size={ 14 }
+						icon={ open ? 'arrow-up-alt2' : 'arrow-down-alt2' }
+					/>
+				</span>
+				{ open && (
+					<span className="ob-editor-options">
+						<ul className="options">
+							{ editorsOrderedFromAPI.map( ( key, index ) => {
+								if ( key === editor ) {
+									return null;
+								}
+								return (
+									<li key={ index }>
+										<a
+											href="#"
+											onClick={ ( e ) => {
+												e.preventDefault();
+												setCurrentEditor( key );
+												setOpen( false );
+											} }
+										>
+											<img
+												className="editor-icon"
+												src={
+													tiobDash.assets +
+													'img/' +
+													EDITOR_MAP[ key ].icon
+												}
+												alt={ __(
+													'Builder Logo',
+													'templates-patterns-collection'
+												) }
+											/>
+										</a>
+									</li>
+								);
+							} ) }
+						</ul>
+					</span>
 				) }
 			</Button>
 		</div>
