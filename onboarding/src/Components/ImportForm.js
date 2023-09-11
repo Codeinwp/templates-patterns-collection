5ac70df839b59d11d679f7443f4c08a3f8be06b4
cristian-ungureanu <cristian@themeisle.com>
2023-09-11 18:00:58 +0300

    chore: add checks for email

--- onboarding/src/Components/ImportForm.js
+++ onboarding/src/Components/ImportForm.js
@@ -3,16 +3,12 @@
 import { __ } from '@wordpress/i18n';
 import { TextControl, Button, SelectControl } from '@wordpress/components';
 import { useState } from '@wordpress/element';
-import { ajaxAction } from '../utils/rest';
 
 const ImportForm = () => {
 	const [ email, setEmail ] = useState( tiobDash.emailSubscribe.email || '' );
 	const [ userLevel, setUserLevel ] = useState( '' );
 	const [ buildingFor, setBuildingFor ] = useState( '' );
 	const [ processingSub, setProcessingSub ] = useState( false );
-	const [ skipSubscribe, setSkipSubscribe ] = useState(
-		'yes' === tiobDash?.emailSubscribe?.skipStatus
-	);
 
 	const userLevelMap = [
 		{
@@ -58,31 +54,8 @@ const ImportForm = () => {
 
 	const site = tiobDash.onboarding.homeUrl || '';
 
-	const markSubscribeSkip = ( redirect, data = {} ) => {
-		ajaxAction(
-			tiobDash.emailSubscribe.ajaxURL,
-			'skip_subscribe',
-			tiobDash.emailSubscribe.nonce,
-			data
-		).then( () => {
-			setSkipSubscribe( true );
-			setProcessingSub( false );
-			window.location.href = redirect;
-		} );
-	};
-
 	const viewWebsiteAndSubscribe = () => {
-		if ( skipSubscribe ) {
-			window.location.href = site;
-			return;
-		}
-
 		setProcessingSub( true );
-
-		if ( '' === email.trim() && '' === userLevel && '' === buildingFor ) {
-			markSubscribeSkip( site, { isTempSkip: true } );
-			return;
-		}
 
 		fetch( 'https://api.themeisle.com/tracking/subscribe', {
 			method: 'POST',
@@ -100,12 +73,12 @@ const ImportForm = () => {
 			.then( ( r ) => r.json() )
 			.then( ( response ) => {
 				if ( 'success' === response.code ) {
-					markSubscribeSkip( site );
+					window.location.href = site;
 				}
 			} )
 			?.catch( ( error ) => {
 				console.error( error );
-				markSubscribeSkip( site, { isTempSkip: true } );
+				window.location.href = site;
 			} );
 	};
 
@@ -117,56 +90,61 @@ const ImportForm = () => {
 					'templates-patterns-collection'
 				) }
 			</p>
-			<div className="ob-form-wrap">
-				<TextControl
-					aria-label={ __(
-						'Enter your email',
-						'templates-patterns-collection'
-					) }
-					type="email"
-					value={ email }
-					onChange={ setEmail }
-				/>
-				<SelectControl
-					className="ob-select"
-					options={ userLevelMap }
-					value={ userLevel }
-					onChange={ setUserLevel }
-				/>
-				<SelectControl
-					className="ob-select"
-					options={ buildingForMap }
-					value={ buildingFor }
-					onChange={ setBuildingFor }
-				/>
-			</div>
-			<div className="ob-done-actions">
-				<Button
-					isPrimary
-					className="ob-button full"
-					disabled={
-						processingSub ||
-						( ! email && ! userLevel && ! buildingFor )
-					}
-					onClick={ viewWebsiteAndSubscribe }
-				>
-					{ __(
-						'Submit and view site',
-						'templates-patterns-collection'
-					) }
-				</Button>
-				<Button
-					isLink
-					className="close is-grayed"
-					href="/"
-					disabled={ processingSub }
-				>
-					{ __(
-						'Skip and view site',
-						'templates-patterns-collection'
-					) }
-				</Button>
-			</div>
+			<form
+				className="ob-subscribe-form"
+				onSubmit={ viewWebsiteAndSubscribe }
+			>
+				<div className="ob-form-wrap">
+					<TextControl
+						aria-label={ __(
+							'Enter your email',
+							'templates-patterns-collection'
+						) }
+						type="email"
+						value={ email }
+						onChange={ setEmail }
+					/>
+					<SelectControl
+						className="ob-select"
+						options={ userLevelMap }
+						value={ userLevel }
+						onChange={ setUserLevel }
+					/>
+					<SelectControl
+						className="ob-select"
+						options={ buildingForMap }
+						value={ buildingFor }
+						onChange={ setBuildingFor }
+					/>
+				</div>
+				<div className="ob-done-actions">
+					<Button
+						type="submit"
+						isPrimary
+						className="ob-button full"
+						disabled={
+							processingSub ||
+							( ! email && ! userLevel && ! buildingFor )
+						}
+					>
+						{ __(
+							'Submit and view site',
+							'templates-patterns-collection'
+						) }
+					</Button>
+					<Button
+						isLink
+						className="close is-grayed"
+						href="/"
+						disabled={ processingSub }
+					>
+						{ __(
+							'Skip and view site',
+							'templates-patterns-collection'
+						) }
+					</Button>
+				</div>
+			</form>
 		</>
 	);
 };
