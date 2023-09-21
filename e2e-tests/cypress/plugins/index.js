f6c83a539906eec1d040c7136459039d415ac437
cristian-ungureanu <cristian@themeisle.com>
2023-09-21 19:48:43 +0300

    chore: fix cypress

--- e2e-tests/cypress/plugins/index.js
+++ e2e-tests/cypress/plugins/index.js
@@ -8,15 +8,12 @@ module.exports = (on) => {
   //	addMatchImageSnapshotPlugin(on, config);
 
   on('before:browser:launch', (browser, launchOptions) => {
-    if (browser.name === 'chrome' && browser.isHeadless) {
-      const headlessIndex = launchOptions.args.indexOf('--headless');
-      if (headlessIndex > -1) {
-        launchOptions.args[headlessIndex] = '--headless=new';
-      }
-      launchOptions.args.push('--version=116');
+    if (browser.name === 'chrome') {
+      launchOptions.args.push('--version=99');
       launchOptions.args.push('--window-size=1366,768');
       launchOptions.args.push('--force-device-scale-factor=1');
     }
+
     return launchOptions;
   });
 };
