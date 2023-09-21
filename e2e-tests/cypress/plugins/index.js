ad47c29375414e7f5948d240c01e6c1b092a74e3
cristian-ungureanu <cristian@themeisle.com>
2023-09-21 18:41:40 +0300

    chore: fix cypress

--- e2e-tests/cypress/plugins/index.js
+++ e2e-tests/cypress/plugins/index.js
@@ -12,8 +12,8 @@ module.exports = (on) => {
 			launchOptions.args.push('--window-size=1366,768');
 			launchOptions.args.push('--force-device-scale-factor=1');
 			launchOptions.args = launchOptions.args.map((arg) => {
-				if (arg === '--headless=new') {
-					return '--headless'
+				if (arg === '--headless') {
+					return '--headless=new'
 				}
 
 				return arg
