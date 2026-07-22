f493e145b4e8b976a10fa5827d8fc6afbf755f80
girishpanchal30 <girish@krishaweb.com>
2026-07-22 13:12:11 +0530

    fix: optimize sites listing caching

--- includes/Main.php
+++ includes/Main.php
@@ -99,7 +99,6 @@ class Main {
 			$this->setup_beaver();
 			$this->setup_elementor();
 		}
-		$this->setup_sites_listing();
 		add_filter( 'themeisle_sdk_hide_dashboard_widget', '__return_true' );
 		add_filter(
 			'templates_patterns_collection_feedback_review_message',
@@ -112,6 +111,7 @@ class Main {
 		if ( ! $this->should_load() ) {
 			return;
 		}
+		$this->setup_sites_listing();
 		$this->setup_admin();
 		$this->setup_api();
 		$this->setup_active_state();
